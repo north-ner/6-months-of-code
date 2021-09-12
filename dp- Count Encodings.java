83fd8b528aab0dd71e77393abb67483b9de065bb
northner <greatdevsingh@gmail.com>
2021-09-12 19:38:51 +0530

    Update dp- Count Encodings.java

--- dp- Count Encodings.java
+++ dp- Count Encodings.java
@@ -52,5 +52,20 @@ public class Main {
     return ans;
 
   }
+  //tabulation ..............................................................................
+  public static int tav(String str){
+      int[]dp=new int[   str.length()+1];
+      dp[str.length()]=1;
+      for(int i=str.length()-1;i>=0;i--){
+          
+               int ch1 = str.charAt(i) - '0';
+    if (ch1 >= 1 && ch1 <= 9) {
+      dp[i]+=dp[i+1];
+    }
+    if (i + 1 < str.length()) {
+      int ch12 = (str.charAt(i) - '0') * 10 + (str.charAt (i + 1) - '0');
+      if (ch12 >= 10 && ch12 <= 26 )
+       dp[i]+=dp[i+2];
+    }
 
 }
