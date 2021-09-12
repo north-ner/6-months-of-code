124e8d2e62a73a71f1dfacc43f5961e5bfc16e0c
northner <greatdevsingh@gmail.com>
2021-09-12 12:14:27 +0530

    Create dp- combination-sum-iv.java

---
+++ dp- combination-sum-iv.java
@@ -0,0 +1,40 @@
+// Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+// The answer is guaranteed to fit in a 32-bit integer.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3], target = 4
+// Output: 7
+// Explanation:
+// The possible combination ways are:
+// (1, 1, 1, 1)
+// (1, 1, 2)
+// (1, 2, 1)
+// (1, 3)
+// (2, 1, 1)
+// (2, 2)
+// (3, 1)
+// Note that different sequences are counted as different combinations.
+
+
+
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+                int [] dp=new int [target +1];
+        dp[0]=1;
+        for(int currenttarget=1;currenttarget<=target;currenttarget++){
+            for(int j=0;j<nums.length;j++)
+            {
+                int remVal=currenttarget-nums[j];
+                if(remVal>=0){
+                    
+                    dp[currenttarget]+=dp[remVal];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
