4274f8c0de552b46b465d5d5ccaa3d39add58cab
northner <greatdevsingh@gmail.com>
2021-09-19 17:57:50 +0530

    Create sq- Balanced Brackets.java

---
+++ sq- Balanced Brackets.java
@@ -0,0 +1,67 @@
+// 1. You are given a string exp representing an expression.
+// 2. You are required to check if the expression is balanced i.e. closing brackets and opening brackets match up well.
+
+// e.g.
+// [(a + b) + {(c + d) * (e / f)}] -> true
+// [(a + b) + {(c + d) * (e / f)]} -> false
+// [(a + b) + {(c + d) * (e / f)} -> false
+// ([(a + b) + {(c + d) * (e / f)}] -> false
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    String str = scn.nextLine();
+    Stack<Character> stk = new Stack<>();
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+
+
+      if (ch == '[' || ch == '{' || ch == '(') {
+        stk.push(ch);
+      } else if (ch == ']') {
+
+        if (stk.isEmpty() == true || stk.peek() != '[')
+        {
+          System.out.println(false);
+          return;
+        }
+        
+        stk.pop();
+        
+      } else if (ch == ')') {
+        if (stk.isEmpty() == true || stk.peek() != '(')
+        {
+          System.out.println(false);
+          return;
+        }
+        
+        stk.pop();      
+          
+      } else if (ch == '}') {
+        if (stk.isEmpty() == true || stk.peek() != '{')
+        {
+          System.out.println(false);
+          return;
+        }
+        
+        stk.pop();      
+      }
+    }
+    
+    if(stk.size() > 0){
+        System.out.println(false);
+        return;
+    }
+    
+    System.out.println(true);
+  }
+
+}
