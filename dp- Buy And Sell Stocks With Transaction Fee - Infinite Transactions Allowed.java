ef923a8c93d5c4fab52ebdd618b3063f4a7d7589
northner <greatdevsingh@gmail.com>
2021-09-19 11:33:19 +0530

    Create dp- Buy And Sell Stocks With Transaction Fee - Infinite Transactions Allowed.java

---
+++ dp- Buy And Sell Stocks With Transaction Fee - Infinite Transactions Allowed.java
@@ -0,0 +1,56 @@
+// 1. You are given a number n, representing the number of days.
+// 2. You are given n numbers, where ith number represents price of stock on ith day.
+// 3. You are give a number fee, representing the transaction fee for every transaction.
+// 4. You are required to print the maximum profit you can make if you are allowed infinite transactions, but has to pay "fee" for every closed transaction.
+// Note - There can be no overlapping transaction. One transaction needs to be closed (a buy followed by a sell) before opening another transaction (another buy).
+// Sample Input
+// 12
+// 10
+// 15
+// 17
+// 20
+// 16
+// 18
+// 22
+// 20
+// 22
+// 20
+// 23
+// 25
+// 3
+// Sample Output
+// 13
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+         Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int []cost = new int [n];
+    for (int i = 0; i < n; i++) {
+      cost[i] = scn.nextInt();
+    }
+    int fees=scn.nextInt();
+    int [][]dp = new int[n][2];
+    //0th col-> extra but ,1t col-> complete sell
+    dp[0][0] = -cost[0];
+    dp[0][1] = 0;
+
+    for (int i = 1; i < n; i++) {
+      //extra buy
+      dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - cost[i]);
+
+      //complete sell
+      dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + cost[i]-fees);
+
+
+    }
+    System.out.println(dp[n - 1][1]);   
+    }
+
+}
